e539e84ced908c6d8e02a799adae824156d07c97
GlebShekovtsov <sanlpop1234@gmail.com>
2022-09-30 16:02:32 +0300

    готов адаптив до планшетов включительно

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
+let burger = document.querySelector(".burger");
+let menu = document.querySelector(".header__nav");
+let menuLinks = menu.querySelectorAll(".nav__link");
+
+burger.addEventListener("click", function () {
+  menu.classList.toggle("header__nav--active");
+  burger.classList.toggle("burger--active");
+  document.body.classList.toggle("scroll-lock");
+});
+
+menuLinks.forEach(function (el) {
+  el.addEventListener("click", function () {
+    menu.classList.remove("header__nav--active");
+    burger.classList.remove("burger--active");
+    document.body.classList.remove("scroll-lock");
+  });
+});
+
+
 
 $(".accordion").accordion({
     active: false,
